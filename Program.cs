0bfd2696bf7d8f4e1f830b599f92766299fee31f
serifahmetharas <91914710+serifahmetharas@users.noreply.github.com>
2021-12-13 17:03:05 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace RecapDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Logger = new FileLogger(); // Buradan ileride Database,File,Sms gibi çeşitli yöntemlerle göndermeyi mümkün kıldık.
+            customerManager.Add();
+
+            Console.ReadLine();
+        }
+
+
+
+
+        class CustomerManager // Burası kullanım kısmı.
+        {
+            public ILogger Logger { get; set; }
+            public void Add()
+            {
+
+                Logger.Log();
+                Console.WriteLine("Customer Added.");
+            }
+
+        }
+
+        class DatabaseLogger : ILogger
+        {
+            public void Log()
+            {
+                Console.WriteLine("Logged to database!");
+            }
+
+        }
+            class FileLogger : ILogger
+            {
+                public void Log()
+                {
+                    Console.WriteLine("Logged to file.");
+                }
+            }
+
+            interface ILogger
+            {
+                public void Log();
+            }
+
+        }
+    }
